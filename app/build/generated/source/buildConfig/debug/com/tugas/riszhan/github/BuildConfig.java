a5801aafe1fe242b4152fe09298bf4b19461cf84
Riszhan <riszhan.n@students.amikom.ac.id>
2021-01-19 20:38:46 +0700

    tugas uas

---
+++ app/build/generated/source/buildConfig/debug/com/tugas/riszhan/github/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.tugas.riszhan.github;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.tugas.riszhan.github";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String GithubAPI = "7e5f26491b034e054fe1cded505a3ebbaa024978";
+}
---
+++ app/src/main/java/com/tugas/riszhan/github/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tugas.riszhan.github.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tugas.riszhan.github.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
